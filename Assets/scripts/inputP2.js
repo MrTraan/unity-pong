8f4692c67819ab827b9423b169e84da1fba8736f
Nathan <nathan@batch.com>
2015-03-21 17:57:31 +0100

    Swag

---
+++ Assets/scripts/inputP2.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var defaultSpeed = new float();
+defaultSpeed = 0.1;
+var maxHeight=4;
+var minHeight=-4;
+function Start () {
+
+}
+
+function Update () {
+	onKeyDown();
+}	
+
+function onKeyDown(){
+	if (Input.GetKey("z")){
+		if (transform.position.y < 4){
+			transform.position += new Vector2(0f, defaultSpeed);
+		}
+	} else if (Input.GetKey("s")){
+		if (transform.position.y > -4){
+			transform.position -= new Vector2(0f, defaultSpeed);
+		}
+	}
+}
